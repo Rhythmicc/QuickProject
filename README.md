dbd9b27130025e7111a139e49e002bb42ff1ebde
连浩丞 <RhythmLian@outlook.com>
2019-10-29 16:25:46 +0800

    支持创建Qpro项目

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # QuickProject
 ## Project description
 
-[![](https://img.shields.io/badge/version-0.5.2.2-green)]()
-[![](https://img.shields.io/badge/Author-RhythmLian-blue)]()
+[![](https://img.shields.io/badge/version-0.5.4-green)]() [![](https://img.shields.io/badge/Author-RhythmLian-blue)]()
+
+### 环境
+
+- Qpro基于`gcc/g++/clang/...`与`python3`,
+- 请确保电脑在拥有Python环境的同时, 至少安装了一套C/CPP编译指令.
+
+### 功能
+
+- 提供脚本在命令行里高效***运行C/CPP项目***。
+- 提供***模板管理器***，轻松将代码保存和导出。
+- 提供***对拍器***，轻松进行程序输出结果的对拍或进行文本文件的对比。
+
+### 特殊说明
+
+- **Qpro对CLion的特殊支持: 在CLion项目中，Qpro可以自动将其初始化为Qpro项目**
+
+- **任意一个包含C/CPP文件的文件夹都可以成为Qpro项目, Qpro不会对原IDE项目产生影响**
+
+- **一个文件夹可以同时是Qpro项目和其他IDE项目**
+
 
 ## 安装:
 
@@ -12,14 +31,15 @@
 
 | Command | Result |
 | :----- | :----- |
-| `Qpro -init` | 为项目添加[配置表](#配置表)和相关文件，令下面命令可执行 |
+| `Qpro -init` | 将当前文件夹初始化为Qpro项目 |
 | `Qpro -h` | 查看帮助 |
 | `Qpro -update` | 更新Qpro |
+| `Qpro -c project` | 创建一个Qpro项目 |
 | `tmpm *` | 调用[模板脚本](#模板脚本)，刷新项目中的默认源文件 |
 | `run *` | 使用[运行脚本](#运行脚本)运行你的项目 |
 | `detector [-[p/f][p/f]]` | 运行[对拍器](#对拍器)（对拍器只能在`Qpro -init`后的文件夹下运行） |
 
-添加配置表后的项目，可以手动编辑`project_configure.csv`来调整配置。
+初始化后的项目，可以手动编辑配置表`project_configure.csv`来调整配置。
 
 ### 配置表
 
@@ -31,7 +51,7 @@
       | `compile_filename` | 待编译的文件 | main.cpp / main.c |
       | `executable_filename` | 编译出的可执行文件名 | 项目名 |
       | `input_file` | 默认的输入文件 | `./cmake-build-debug/input.txt`|
-      | `template_root` | 默认的模板根目录 | `template/` |
+      | `template_root` | 默认的模板库根目录 | `template/` |
 
 ### 运行脚本:
 
@@ -51,18 +71,18 @@
 
   - 输入输出:
     
-      - 可以编辑 `./cmake-build-debug/input.txt` 来设置默认输入
+      - 可以编辑 **默认的输入文件** 来设置默认输入
       - 运行 `run [...] > output.txt` 使程序输出到 `./output.txt`
       
       | 参数 | 效果 |
       | :----- | :----- |
-      | -i | 使用`./cmake-build-debug/input.txt`作为输入 |
+      | -i | 使用**默认的输入文件**作为输入 |
       | -if `*.*` | 更改输入文件 |
       | -if -paste | 使用粘贴板内容作为输入 |
       
-  - 程序的额外命令行参数:
+  - ***程序的额外命令行参数:***
   
-      - 在符合上述命令规则情况下，你可以在任意位置加入参数，这些参数将传递给编译出的程序。
+      - ***所有未被run命令匹配的参数都将按顺序传递给源程序。***
       
   - 查看帮助
     
@@ -80,7 +100,7 @@
 ### 模板脚本:
 
 - 使用
- 
+
   | 命令 | 效果 |
   |:---|:---|
   | `tmpm` | 刷新项目中的默认源文件 |
@@ -93,12 +113,13 @@
 ### 对拍器
 
   - 使用: `detector` 来进行两个源程序运行结果的对拍，结果存储在当前目录下的`./res.html`。
-  
-  | 命令 | 效果 |
-  |:---|:---|
-  | `detector (-pp)` | 打开默认对拍器(`文件1`与`文件2`都是程序) |
-  | `detector -pf` | 运行`文件1`程序并将结果与`文件2`对比 |
-  | `detector -fp` | 运行`文件2`程序并将结果与`文件1`对比 |
-  | `detector -ff` | 对比`文件1`与`文件2`的内容(你可以在任何位置调用)|
+
+| 命令 | 效果 |
+|:---|:---|
+| `detector (-pp)` | 打开默认对拍器(`文件1`与`文件2`都是程序) |
+| `detector -pf` | 运行`文件1`程序并将结果与`文件2`对比 |
+| `detector -fp` | 运行`文件2`程序并将结果与`文件1`对比 |
+| `detector -ff` | 对比`文件1`与`文件2`的内容(你可以在任何位置调用)|
   - 如果脚本未能自动打开`./res.html`, 你可以用浏览器打开它。
-    ![GUI](https://github.com/Rhythmicc/QuickProject/blob/master/img/1.png?raw=true)
+
+![GUI](https://github.com/Rhythmicc/QuickProject/blob/master/img/1.png?raw=true)
--- build/lib/QuickProject/Qpro.py
+++ build/lib/QuickProject/Qpro.py
@@ -8,34 +8,82 @@ if sys.platform.startswith('win'):
 else:
     is_win = False
     dir_char = '/'
+work_dir = os.getcwd() + dir_char
+
+cpp_template = """
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    return 0;    
+}
+"""
+
+c_template = """
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    return 0;    
+}
+"""
 
 
 def main():
     if '-h' in sys.argv:
         print('usage:\n'
-              '\t * [Qpro -init   ]: add configure so that command \"Qpro*\" can work\n'
+              '\t * [Qpro -init   ]: let dir be a Qpro project!\n'
               '\t * [Qpro -h      ]: help\n'
               '\t * [Qpro -update ]: update Qpro\n'
+              '\t * [Qpro -c name ]: create a Qpro project\n'
               '\t * [tmpm *       ]: manage your template\n'
-              '\t * [run *        ]: run your C/CPP project\n'
+              '\t * [run *        ]: run your Qpro project\n'
               '\t * [detector -[p/f][p/f] ]: run beat detector for two source files')
         exit(0)
     elif '-update' in sys.argv:
         os.system('pip3 install Qpro --upgrade')
         exit(0)
+    elif '-c' in sys.argv:
+        try:
+            project_name = sys.argv[sys.argv.index('-c')+1]
+        except IndexError:
+            exit('usage: Qpro -c project')
+        else:
+            os.mkdir(project_name)
+            is_cpp = True if input('Is a cpp project? [y/n]:') == 'y' else False
+            source_file = 'main.cpp' if is_cpp else 'main.c'
+            with open(project_name+dir_char+source_file, 'w') as f:
+                f.write(cpp_template if is_cpp else c_template)
+            os.mkdir(project_name+dir_char+'dist')
+            os.mkdir(project_name+dir_char+'template')
+            info = [
+                ['compile_tool', 'g++ -std=c++11' if is_cpp else 'gcc -std=c11', ''],
+                ['compile_filename', source_file],
+                ['executable_filename', 'dist'+dir_char+project_name],
+                ['input_file', 'dist'+dir_char+'input.txt'],
+                ['template_root', 'template' + dir_char]
+            ]
+            with open(project_name+dir_char+'project_configure.csv', 'w') as f:
+                for row in info:
+                    f.write(','.join(row) + '\n')
+            with open(project_name+dir_char+info[3][-1], 'w') as f:
+                f.write('edit this file to make input')
+            with open(project_name+dir_char+info[1][-1], 'r') as f:
+                main_cont = f.read()
+            with open(project_name+dir_char+'template' + dir_char + 'main', 'w') as f:
+                f.write(main_cont)
     elif '-init' not in sys.argv:
         exit('wrong usage! Run "Qpro -h" for help!')
-    if not os.path.exists('project_configure.csv'):
-        work_dir = os.getcwd() + dir_char
+    elif not os.path.exists('project_configure.csv'):
         if not os.path.exists('CMakeLists.txt'):
-            print("Not an CLion Project! You need make configure manually [y/n]:", end='')
-            ask = input()
+            ask = input('Not an CLion Project!' + 'You need make configure manually [y/n]:')
             if 'y' not in ask and 'Y' not in ask:
                 exit(0)
             is_cpp = True if input('Is a cpp project? [y/n]:') == 'y' else False
             project_name = os.path.abspath(input('Set executable file path:')).replace(work_dir, '')
             if dir_char not in project_name:
-                project_name = '.'+dir_char+project_name
+                project_name = '.' + dir_char + project_name
             source_file = 'main.c' + ('pp' if is_cpp else '')
             while not os.path.exists(source_file):
                 source_file = input('Not found "%s", set compile_filename:' % source_file)
@@ -51,7 +99,7 @@ def main():
             project_name = 'cmake-build-debug' + dir_char + project_name
             source_file = 'main.c' + ('pp' if is_cpp else '')
         pro_root = dir_char.join(project_name.split(dir_char)[:-1])
-        default_input = pro_root+dir_char+'input.txt'
+        default_input = pro_root + dir_char + 'input.txt'
         print('adding project_configure')
         info = [
             ['compile_tool', 'g++ -std=c++11' if is_cpp else 'gcc -std=c11', ''],
@@ -63,11 +111,11 @@ def main():
         with open('project_configure.csv', 'w') as f:
             for row in info:
                 f.write(','.join(row) + '\n')
-        with open(default_input, 'w') as f:
+        with open(info[3][-1], 'w') as f:
             f.write('edit this file to make input')
         if not os.path.exists('template') or not os.path.isdir('template'):
             os.mkdir('template')
-        with open(source_file, 'r') as f:
+        with open(info[1][-1], 'r') as f:
             main_cont = f.read()
         with open('template' + dir_char + 'main', 'w') as f:
             f.write(main_cont)

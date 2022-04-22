8b161d26012c6fce3eda29e9bf9eaaba2495e299
Rhythmicc <RhythmLian@outlook.com>
2022-04-22 09:22:27 +0800

    update

--- complete/fig/zh/Qpro.ts
+++ complete/fig/zh/Qpro.ts
@@ -1,56 +1,59 @@
 const completionSpec: Fig.Spec = {
   name: 'Qpro',
   description: 'Quick Project',
-  options: [{
-    name: '-init',
+  options: [
+    {
+      name: '-h',
+      description: '帮助'
+    },
+  ],
+  subcommands: [{
+    name: 'init',
     description: '使当前目录成为Qpro项目',
-  }, {
-    name: '-h',
-    description: '帮助'
-  }, {
-    name: '-c',
+  },  {
+    name: 'create',
     description: '创建Qpro项目',
     args: {name: 'name', description: '项目名'}
   }, {
-    name: '-update',
+    name: 'update',
     description: '更新Qpro'
   }, {
-    name: '-adjust',
+    name: 'adjust',
     description: '调整配置表'
   }, {
-    name: '-ssh',
+    name: 'ssh',
     description: '通过SSH登录远程映射'
   }, {
-    name: '-scp',
+    name: 'scp',
     description: '上传路径到默认的远程映射对应位置',
-    args: {name: 'path', description: '路径', template: ['filepaths', 'folders']}
+    args: {name: 'path', description: '路径', template: ["filepaths", "folders"]}
   }, {
-    name: '-smv',
+    name: 'smv',
     description: '移动路径到默认的远程映射对应位置',
-    args: {name: 'path', description: '路径', template: ['filepaths', 'folders']}
+    args: {name: 'path', description: '路径', template: ["filepaths", "folders"]}
   }, {
-    name: '-scp-init',
+    name: 'scp-init',
     description: '上传当前全部内容到远程映射'
   }, {
-    name: '-get',
+    name: 'get',
     description: '从远程映射下载',
-    args: {name: 'path', description: '路径', template: ['filepaths', 'folders']}
+    args: {name: 'path', description: '路径', template: ["filepaths", "folders"]}
   }, {
-    name: '-del',
+    name: 'del',
     description: '同时删除本地及远程映射文件或目录',
-    args: {name: 'path', description: '路径', template: ['filepaths', 'folders']}
+    args: {name: 'path', description: '路径', template: ["filepaths", "folders"]}
   }, {
-    name: '-del-all',
+    name: 'del-all',
     description: '同时删除本地及远程映射文件或目录'
   }, {
-    name: '-ls',
+    name: 'ls',
     description: '展示路径中的子项',
-    args: {name: 'path', description: '路径', template: ['filepaths', 'folders']}
+    args: {name: 'path', description: '路径', template: ["filepaths", "folders"]}
   }, {
-    name: '-enable-complete',
+    name: 'enable-complete',
     description: '支持Commander应用的自动补全'
   }, {
-    name: '-csv',
+    name: 'csv',
     description: '将旧的Qpro配置表改为json格式'
   }],
   // Only uncomment if Qpro takes an argument

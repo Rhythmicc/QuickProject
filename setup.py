84fa9426a98a1b6d9f987310d9f79be9b7453a89
Rhythmicc <RhythmLian@outlook.com>
2022-07-21 02:37:39 +0800

    支持多文件命令行APP注册为全局命令

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-VERSION = '0.9.14'
+VERSION = '0.9.15'
 
 setup(
     name='Qpro',
